d83b2e7afc794ddfc033ca7884016cbc136acbf5
Camila Brito <camilam.brito@outlook.com.br>
2025-09-24 21:58:21 -0300

    Criando conexao e buscando dados do banco mysql.

--- index.php
+++ index.php
@@ -1,56 +1,13 @@
 <?php
-    $_produtos_cafe = [
-            [
-                'nome'      => "Café Cremosos",
-                'descricao' => "Café cremoso irresistivelmente suave e que envolve seu paladar",
-                'preco'     => "5.00",
-                'imagem'    => "img/cafe-cremoso.jpg"
-            ],
-            [
-                'nome'      => "Café com Leite",
-                'descricao' => "A harmonia perfeita do café e do leite, uma experiência reconfortante",
-                'preco'     => "2.00",
-                'imagem'    => "img/cafe-com-leite.jpg"
-            ],
-            [
-                'nome'      => "Cappuccino",
-                'descricao' => "Café suave, leite cremoso e uma pitada de sabor adocicado",
-                'preco'     => "7.00",
-                'imagem'    => "img/cappuccino.jpg"
-            ],
-            [
-                'nome'      => "Café Gelado",
-                'descricao' => "Café gelado refrescante, adoçado e com notas sutis de baunilha ou caramelo.",
-                'preco'     => "3.00",
-                'imagem'    => "img/cafe-gelado.jpg"
-            ]
-    ];
-    $_produtos_almoco = [
-        [
-            'nome'      => "Bife",
-            'descricao' => "Bife, arroz com feijão e uma deliciosa batata frita",
-            'preco'     => "27.90",
-            'imagem'    => "img/bife.jpg"
-        ],
-        [
-            'nome'      => "Filé de peixe",
-            'descricao' => "Filé de peixe salmão assado, arroz, feijão verde e tomate.",
-            'preco'     => "24.99",
-            'imagem'    => "img/prato-peixe.jpg"
-        ],
-        [
-            'nome'      => "Frango",
-            'descricao' => "Saboroso frango à milanesa com batatas fritas, salada de repolho e molho picante",
-            'preco'     => "23.00",
-            'imagem'    => "img/prato-frango.jpg"
-        ],
-        [
-            'nome'      => "Fettuccine",
-            'descricao' => "Prato italiano autêntico da massa do fettuccine com peito de frango grelhado",
-            'preco'     => "22.50",
-            'imagem'    => "img/fettuccine.jpg"
-        ]
-    ];
+
+    global $_pdo;
+    require "src/conexao-bd.php";
+
+    $_statement = $_pdo->query("SELECT * FROM produtos WHERE tipo = 'Café' ORDER BY preco;");
+    $_produtos_cafe = $_statement->fetchAll(PDO::FETCH_ASSOC);
+
+    $_statement = $_pdo->query("SELECT * FROM produtos WHERE tipo = 'Almoço' ORDER BY preco;");
+    $_produtos_almoco = $_statement->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 <!doctype html>
@@ -86,7 +43,7 @@
                 <?php foreach ($_produtos_cafe as $produto): ?>
                 <div class="container-produto">
                     <div class="container-foto">
-                        <img src="<?= $produto['imagem'] ?>">
+                        <img src="<?= "img/".$produto['imagem'] ?>">
                     </div>
                     <p><?= $produto['nome'] ?></p>
                     <p><?= $produto['descricao'] ?></p>
@@ -104,7 +61,7 @@
                 <?php foreach ($_produtos_almoco as $produto): ?>
                 <div class="container-produto">
                     <div class="container-foto">
-                        <img src="<?= $produto['imagem'] ?>">
+                        <img src="<?= "img/".$produto['imagem'] ?>">
                     </div>
                     <p><?= $produto['nome'] ?></p>
                     <p><?= $produto['descricao'] ?></p>
---
+++ src/conexao-bd.php
@@ -0,0 +1,5 @@
+<?php
+
+$_user = "camila_brito";
+$_password = "1234";
+$_pdo = new PDO("mysql:host=localhost;dbname=serenatto", $_user, $_password);
